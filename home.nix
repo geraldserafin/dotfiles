8b32e1f02d7ee8f9e498b93da95ea1b240ebdaad
geraldserafin <serafingerald@protonmail.com>
2024-06-28 17:02:56 +0200

    Improve home.nix file format

--- home.nix
+++ home.nix
@@ -1,43 +1,6 @@
 { pkgs, ... }:
 
 {
-  home.username = "gerald";
-  home.stateVersion = "23.11";
-  home.sessionVariables = {
-    TERMINAL = "alacritty";
-    BROWSER = "firefox";
-  };
-
-  home.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-    xmobar
-    firefox
-    killall
-    telegram-desktop
-    pavucontrol
-    gcc
-    lua-language-server
-    stylua
-    nixd
-    nixfmt-classic
-    zsh-powerlevel10k
-    spotify
-    postman
-    discord
-    lazygit
-    obsidian
-    google-chrome
-    ripgrep
-    rofi-screenshot
-    maim
-    xclip
-    tor
-    steam
-    feh
-  ];
-
-  home.keyboard = { variant = "workman"; };
-
   imports = [
     (import ./modules/wallpaper)
     (import ./modules/xmonad)
@@ -49,6 +12,48 @@
     (import ./modules/tmux)
     (import ./modules/rofi)
   ];
+
+  home = {
+    username = "gerald";
+    stateVersion = "23.11";
+    sessionVariables = {
+      TERMINAL = "alacritty";
+      BROWSER = "firefox";
+    };
+    keyboard.variant = "workman";
+    packages = with pkgs; [
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+      xmobar
+      firefox
+      killall
+      telegram-desktop
+      pavucontrol
+      gcc
+      lua-language-server
+      stylua
+      nixd
+      nixfmt-classic
+      zsh-powerlevel10k
+      spotify
+      postman
+      discord
+      lazygit
+      obsidian
+      google-chrome
+      ripgrep
+      rofi-screenshot
+      maim
+      xclip
+      tor-browser
+      feh
+      openpomodoro-cli
+    ];
+  };
+
+  services.picom = {
+    enable = true;
+    vSync = true;
+  };
 
   programs.home-manager.enable = true;
   nixpkgs.config.allowUnfree = true;
