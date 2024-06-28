0d0a3aac7e4dc49bd824eb58707624ef17becf60
geraldserafin <serafingerald@protonmail.com>
2024-06-28 17:01:36 +0200

    Cleanup configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,50 +4,55 @@
   imports =
     [ ./hardware-configuration.nix inputs.home-manager.nixosModules.default ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 
-  networking.hostName = "babylon";
+  networking = {
+    hostName = "babylon";
+    networkmanager.enable = true;
+  };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  services.picom = {
-    enable = true;
-    vSync = true;
+  sound.enable = true;
+
+  hardware.pulseaudio.enable = true;
+
+  nixpkgs.config = {
+    pulseaudio = true;
+    allowUnfree = true;
   };
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  nixpkgs.config.pulseaudio = true;
-
-  services.xserver = {
-    enable = true;
-    displayManager.session = [{
-      manage = "desktop";
-      name = "xsession";
-      start = "";
-    }];
-    xkb = {
-      layout = "us,pl";
-      variant = "workman,";
+  services = {
+    xserver = {
+      enable = true;
+      displayManager.session = [{
+        manage = "desktop";
+        name = "xsession";
+        start = "";
+      }];
     };
   };
 
-  networking.networkmanager.enable = true;
+  programs = { steam.enable = true; };
 
   time.timeZone = "Europe/Warsaw";
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pl_PL.UTF-8";
-    LC_IDENTIFICATION = "pl_PL.UTF-8";
-    LC_MEASUREMENT = "pl_PL.UTF-8";
-    LC_MONETARY = "pl_PL.UTF-8";
-    LC_NAME = "pl_PL.UTF-8";
-    LC_NUMERIC = "pl_PL.UTF-8";
-    LC_PAPER = "pl_PL.UTF-8";
-    LC_TELEPHONE = "pl_PL.UTF-8";
-    LC_TIME = "pl_PL.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "pl_PL.UTF-8";
+      LC_IDENTIFICATION = "pl_PL.UTF-8";
+      LC_MEASUREMENT = "pl_PL.UTF-8";
+      LC_MONETARY = "pl_PL.UTF-8";
+      LC_NAME = "pl_PL.UTF-8";
+      LC_NUMERIC = "pl_PL.UTF-8";
+      LC_PAPER = "pl_PL.UTF-8";
+      LC_TELEPHONE = "pl_PL.UTF-8";
+      LC_TIME = "pl_PL.UTF-8";
+    };
   };
 
   console.keyMap = "pl2";
@@ -61,8 +66,6 @@
     extraSpecialArgs = { inherit inputs; };
     users = { "gerald" = import ./home.nix; };
   };
-
-  nixpkgs.config.allowUnfree = true;
 
   system.stateVersion = "23.11";
 }
