7ceeae653ece3b72146dbeff63df03c42efe91c9
geraldserafin <serafingerald@protonmail.com>
2024-07-06 08:54:30 +0200

    Theme notifications

--- modules/dunst/default.nix
+++ modules/dunst/default.nix
@@ -1,5 +1,16 @@
 { ... }:
 
 {
-  services.dunst = { enable = true; };
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        origin = "bottom-right";
+        foreground = "#959595";
+        background = "#292929";
+        frame_color = "#3c3c3c";
+        font = "JetBrainsMono Nerd Font 9";
+      };
+    };
+  };
 }
